490df7e846b54f0ad040c4f8169cd3e35679bcd4
Kristofer Rye <kristofer.rye@gmail.com>
2013-08-09 14:35:22 -0400

    adding form-y stuff

--- assets/js/scoutify.js
+++ assets/js/scoutify.js
@@ -2,23 +2,28 @@ var fs = require('fs');
 var path = require('path');
 
 function getUserHome() {
-  return process.env.HOME /*|| process.env.USERPROFILE*/;
+	return process.env.HOME /*|| process.env.USERPROFILE*/;
 }
 
 function writeToFile(whereToWrite, thingToWrite) {
-  whereToWrite += '.txt';
-  fs.mkdir(path.join(getUserHome(), '.scoutify'));
-  fs.writeFile(path.join(getUserHome(), '.scoutify', whereToWrite), thingToWrite, function(err) {
-    if(err) {
-      console.log(err);
-    } else {
-      console.log("The file was saved!");
-    }
-  }); 
+	whereToWrite += '.txt';
+	fs.mkdir(path.join(getUserHome(), '.scoutify'));
+	fs.writeFile(path.join(getUserHome(), '.scoutify', whereToWrite), thingToWrite, function(err) {
+		if(err) {
+			console.log(err);
+		} else {
+			console.log("The file was saved!");
+		}
+	}); 
+}
+
+function generate_json_string() {
+	team_colors = ["red", "blue"];
+	fancy_object = {};
+	console.log("GJS called!\n");
 }
 
 $(".writefilebutton").click(function() {
     var t = $('textarea').val();
     writeToFile(t.substr(0,6), t);
-}); 
-
+});
